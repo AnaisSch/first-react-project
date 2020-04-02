df53c7ee92e0c3a6783085b5678fe60cdb8d1654
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-04-02 17:04:59 +0200

    add Navbar

--- src/App.js
+++ src/App.js
@@ -5,19 +5,38 @@ import {
   Route,
 } from 'react-router-dom';
 
-import Home          from './pages/Home';
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Home from './pages/Home';
 import CreateArticle from './pages/CreateArticle';
 import DeleteArticle from './pages/DeleteArticle';
-import NotFound      from './pages/NotFound';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Router>
+      <Nav>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/">
+            Accueil
+                    </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/articles/create">
+            Créer un article
+                    </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/articles/delete">
+            Supprimer un article
+                    </Nav.Link>
+        </Nav.Item>
+      </Nav>
       <Switch>
-        <Route exact path="/"           component={Home} />
-        <Route path="/articles/create/" component={CreateArticle} /> 
+        <Route exact path="/" component={Home} />
+        <Route path="/articles/create/" component={CreateArticle} />
         <Route path="/articles/delete/" component={DeleteArticle} />
-        <Route path="*"                 component={NotFound} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
+            
             <h1>Page d'accueil</h1>
         </div>
     );
