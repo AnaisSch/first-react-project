ddf968899471780d0cd9bc3357910374d9d249fe
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-04-02 11:01:18 +0200

    add content and author state on CreateArticle

--- src/CreateArticle.js
+++ src/CreateArticle.js
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
 
 const CreateArticle = () => {
-    const [ title, setTitle ] = useState("");
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [author, setAuthor] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("titre", title);
+        console.log("content", content);
+        console.log("author", author);
     }
     const handleChange = (event) => {
         console.log("Target name: ", event.target.name);
         console.log("Target value: ", event.target.value);
 
-        setTitle(event.target.value.substring(0, 10));
+        switch (event.target.name) {
+            case "title":
+                setTitle(event.target.value);
+                break;
+            case "content":
+                setContent(event.target.value);
+                break;
+            case "author":
+                setAuthor(event.target.value);
+                break;
+            // no default
+        }
     }
+
     return (
         <form onSubmit = {handleSubmit}>
             <fieldset>
@@ -29,17 +45,22 @@ const CreateArticle = () => {
                 <div>
                     <label>Ecrivez un contenu :</label>
                     <textarea
+                        name="content"
+                        onChange={handleChange}
+                        value={content}
                         type="textarea"
                         id="content"
-                        name="content">
-                    </textarea>
+                    />
                 </div>
                 <div>
                     <label>Id de l'auteur :</label>
                     <input
+                        name="author"
+                        onChange={handleChange}
+                        value={author}
                         type="text"
                         id="auteur"
-                        name="author" />
+                         />
                 </div>
                 <div>
                     <button type="submit">Envoyer</button>
